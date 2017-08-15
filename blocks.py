b3839a1ffd2fb2b8a8bdf6be509b4e0e229d15b8
Nataliia Iatsuk <iatsukn@gmail.com>
2017-08-15 15:53:25 +0300

    add BlockDie and BlockTeleport

--- blocks.py
+++ blocks.py
@@ -3,11 +3,21 @@
 
 from pygame import *
 import os
+import pyganim
 
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
-PLATFORM_COLOR = "#FF6262"
+PLATFORM_COLOR = "#000000"
 ICON_DIR = os.path.dirname(__file__) #  повний шлях до каталогу з файлами
+
+ANIMATION_BLOCKTELEPORT = [
+            ('%s/blocks/portal2.png' % ICON_DIR),
+            ('%s/blocks/portal1.png' % ICON_DIR)]
+            
+ANIMATION_PRINCESS = [
+            ('%s/blocks/princess_l.png' % ICON_DIR),
+            ('%s/blocks/princess_r.png' % ICON_DIR)]
+            
  
 class Platform(sprite.Sprite):
     def __init__(self, x, y):
@@ -15,4 +25,25 @@ class Platform(sprite.Sprite):
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
         self.image.fill(Color(PLATFORM_COLOR))
         self.image = image.load("%s/blocks/platform.png" % ICON_DIR)
+        self.image.set_colorkey(Color(PLATFORM_COLOR))
         self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+        
+class BlockDie(Platform):
+    def __init__(self, x, y):
+        Platform.__init__(self, x, y)
+        self.image = image.load("%s/blocks/dieBlock.png" % ICON_DIR)
+
+class BlockTeleport(Platform):
+    def __init__(self, x, y, goX,goY):
+        Platform.__init__(self, x, y)
+        self.goX = goX # координати переміщення
+        self.goY = goY
+        boltAnim = []
+        for anim in ANIMATION_BLOCKTELEPORT:
+            boltAnim.append((anim, 0.3))
+        self.boltAnim = pyganim.PygAnimation(boltAnim)
+        self.boltAnim.play()
+        
+    def update(self):
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.boltAnim.blit(self.image, (0, 0))
