555b4b35c9447adca314560165524c0392e6597e
Niats <iatsukn@gmail.com>
2017-08-15 19:22:16 +0300

    update main and add princess

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import pygame
 from pygame import *
 from player import *
 from blocks import *
+from monsters import *
 
 #Оголошуємо змінні
 WIN_WIDTH = 800 #Ширина вікна
-WIN_HEIGHT = 640 #Висота
+WIN_HEIGHT = 500 # Висота
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT) # Групуємо ширну і висоту в одну змінну
-BACKGROUND_COLOR = "#66ccff"
+BACKGROUND_COLOR = "#000000"
+
+FILE_DIR = os.path.dirname(__file__)
 
 class Camera(object):
     def __init__(self, camera_func, width, height):
@@ -32,68 +35,87 @@ def camera_configure(camera, target_rect):
     l = min(0, l)                           # Не рухається дальше лівої границі
     l = max(-(camera.width-WIN_WIDTH), l)   # Не рухається дальше правої границі
     t = max(-(camera.height-WIN_HEIGHT), t) # Не рухається дальше нижньої границі
-    t = min(0, t)                           # Не рухається дальше верхньої границі
+    t = min(0, t)                            # Не рухається дальше верхньої границі
 
-    return Rect(l, t, w, h)        
+    return Rect(l, t, w, h) 
 
+
+def loadLevel():
+    global playerX, playerY #обявляємо глобальні премінні, це координати героя
+
+    levelFile = open('%s/levels/1.txt' % FILE_DIR)
+    line = " "
+    commands = []
+    while line[0] != "/": # завершення файлу
+        line = levelFile.readline() # прядково
+        if line[0] == "[": # початок рівня
+            while line[0] != "]": # кінець рівня
+                line = levelFile.readline()
+                if line[0] != "]": #якщо не має
+                    endLine = line.find("|") # то шукаємо символ кінця рядка
+                    level.append(line[0: endLine]) # добавляємо рівень в рядок відпочатку до символу "|"
+                    
+        if line[0] != "": # якщо рядок не пустий
+         commands = line.split() # розбиваємо його на окремі команди
+         if len(commands) > 1: #якщо к-ть команд > 1, то шукаємо їх
+            if commands[0] == "player": # якщо перша - player
+                playerX= int(commands[1]) # то записуємо координати героя
+                playerY = int(commands[2])
+            if commands[0] == "portal": # якщо перша команда portal, то створюємо
+                tp = BlockTeleport(int(commands[1]),int(commands[2]),int(commands[3]),int(commands[4]))
+                entities.add(tp)
+                platforms.append(tp)
+                animatedEntities.add(tp)
+            if commands[0] == "monster": # якщо команда monster, то створюємо монстра
+                mn = Monster(int(commands[1]),int(commands[2]),int(commands[3]),int(commands[4]),int(commands[5]),int(commands[6]))
+                entities.add(mn)
+                platforms.append(mn)
+                monsters.add(mn)
 
 def main():
+    loadLevel()
     pygame.init() # Ініціалізуємо PyGame
     screen = pygame.display.set_mode(DISPLAY) # Ствоюємо вікно
-    pygame.display.set_caption("Super Mario") # Пишем в шапку
-    bg = Surface((WIN_WIDTH,WIN_HEIGHT)) # Створення видимої поверхні, фон                       
-    bg.fill(Color(BACKGROUND_COLOR))     # Заливаємо поверхню суцільним кольором
-    
-    hero = Player(65,65) # створюємо героя по (x,y) координатам
+    pygame.display.set_caption("Super Mario Boy") # Пишем в шапку
+    bg = Surface((WIN_WIDTH,WIN_HEIGHT)) # Створення видимої поверхні, фон
+                                        
+    bg.fill(Color(BACKGROUND_COLOR))   # Заливаємо поверхню суцільним кольором 
+        
     left = right = False # по замовчуванню - стояти
     up = False
-    
-    entities = pygame.sprite.Group() # Всі обєкти
-    platforms = [] # те, в що ми будемо врізаться чи опиратися
-    
+    running = False
+     
+    hero = Player(playerX,playerY) # створюємо героя по (x,y) координатам
     entities.add(hero)
            
-    level = [
-       "---------------------------------------------",
-       "-                                           -",
-       "-                                           -",
-       "-                   ---                     -",
-       "-                                           -",
-       "---                                         -",
-       "-                                           -",
-       "-               ---         ---             -",
-       "-                                           -",
-       "----                                        -",
-       "-                                           -",
-       "-                           ----            -",
-       "-                                           -",
-       "-                                         ---",
-       "-      ---                                  -",
-       "-                                           -",
-       "-                   ----                    -",
-       "-                                           -",
-       "-                                           -",
-       "---------------------------------------------"]
-       
     timer = pygame.time.Clock()
     x=y=0 # координати
-    for row in level: # вся строка
+    for row in level: # весь рядок
         for col in row: # кожний символ
             if col == "-":
                 pf = Platform(x,y)
                 entities.add(pf)
                 platforms.append(pf)
-
+            if col == "*":
+                bd = BlockDie(x,y)
+                entities.add(bd)
+                platforms.append(bd)
+            if col == "P":
+                pr = Princess(x,y)
+                entities.add(pr)
+                platforms.append(pr)
+                animatedEntities.add(pr)
+   
             x += PLATFORM_WIDTH #блоки платформи ставлятся на ширині блоків
         y += PLATFORM_HEIGHT    #то саме з висотою
         x = 0                   #на кожній новій строці начинаємо з нуля
     
-    total_level_width  = len(level[0])*PLATFORM_WIDTH # Вираховуємо фактичну ширину рівня
+    total_level_width  = len(level[0])*PLATFORM_WIDTH # Вираховуємо фактичну ширину рівня уровня
     total_level_height = len(level)*PLATFORM_HEIGHT   # Висоту
     
     camera = Camera(camera_configure, total_level_width, total_level_height) 
     
-    while 1: # Основний цикл програми
+    while not hero.winner: # Основний цикл програми
         timer.tick(60)
         for e in pygame.event.get(): # Обробляємо події
             if e.type == QUIT:
@@ -104,7 +126,8 @@ def main():
                 left = True
             if e.type == KEYDOWN and e.key == K_RIGHT:
                 right = True
-
+            if e.type == KEYDOWN and e.key == K_LSHIFT:
+                running = True
 
             if e.type == KEYUP and e.key == K_UP:
                 up = False
@@ -112,19 +135,23 @@ def main():
                 right = False
             if e.type == KEYUP and e.key == K_LEFT:
                 left = False
+            if e.type == KEYUP and e.key == K_LSHIFT:
+                running = False
 
-        screen.blit(bg, (0,0))      # Кажну ітерацію потрібно все перемальовути
+        screen.blit(bg, (0,0))      # Кожну ітерацію потрібно все перемальовути 
 
-
-        camera.update(hero) # централізуємо камеру віповідно до героя
-        hero.update(left, right, up,platforms) # рух
-        #entities.draw(screen) # відображення
+        animatedEntities.update() 
+        monsters.update(platforms) 
+        camera.update(hero)  # централізуємо камеру віповідно до героя
+        hero.update(left, right, up, running, platforms) 
         for e in entities:
             screen.blit(e.image, camera.apply(e))
+        pygame.display.update()       # обновлення і вивід всіх змін на екран
         
-        
-        pygame.display.update()     # обновлення і вивід всіх змін на екран
-        
-
+level = []
+entities = pygame.sprite.Group() # всі обєкти
+animatedEntities = pygame.sprite.Group() 
+monsters = pygame.sprite.Group() 
+platforms = [] 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -4,14 +4,19 @@
 from pygame import *
 import pyganim
 import os
+import blocks
+import monsters
 
 MOVE_SPEED = 7
+MOVE_EXTRA_SPEED = 2.5 # пришвидшення
 WIDTH = 22
 HEIGHT = 32
 COLOR =  "#888888"
 JUMP_POWER = 10
-GRAVITY = 0.35 # Сила, яка буде тягнути в низ
+JUMP_EXTRA_POWER = 1  # дадаткова сила прижку
+GRAVITY = 0.35 # Сила, яка буде героя тягнути в низ
 ANIMATION_DELAY = 0.1 # швидкість зміни кадрів
+ANIMATION_SUPER_SPEED_DELAY = 0.05 # швидкість зміни кадрів при пришвидшенні прижка
 ICON_DIR = os.path.dirname(__file__) #  повний шлях до каталогу з файлами
 
 ANIMATION_RIGHT = [('%s/mario/r1.png' % ICON_DIR),
@@ -32,31 +37,39 @@ ANIMATION_STAY = [('%s/mario/0.png' % ICON_DIR, 0.1)]
 class Player(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
-        self.xvel = 0   #швидкість переміщення. 0 - стояти на місці
-        self.startX = x # Начальна позиція Х, якщо будемо перегравати рівень
+        self.xvel = 0   #швидкість переміщення. 0 - стоїть на місці
+        self.startX = x # початкова позиція Х,якщо рівень буде переграватися
         self.startY = y
-        self.yvel = 0 # швидкість вертикального переміщення
-        self.onGround = False # Чи на землі?
+        self.yvel = 0 #швидкість вертикального переміщення
+        self.onGround = False 
         self.image = Surface((WIDTH,HEIGHT))
         self.image.fill(Color(COLOR))
         self.rect = Rect(x, y, WIDTH, HEIGHT) # прямокутний обєкт
-        self.image.set_colorkey(Color(COLOR)) # робить фон прозорим
+        self.image.set_colorkey(Color(COLOR)) # прозорий фон
 #        Анімація руху в право
         boltAnim = []
+        boltAnimSuperSpeed = []
         for anim in ANIMATION_RIGHT:
             boltAnim.append((anim, ANIMATION_DELAY))
+            boltAnimSuperSpeed.append((anim, ANIMATION_SUPER_SPEED_DELAY))
         self.boltAnimRight = pyganim.PygAnimation(boltAnim)
         self.boltAnimRight.play()
-#        Анімація руху в ліво       
+        self.boltAnimRightSuperSpeed = pyganim.PygAnimation(boltAnimSuperSpeed)
+        self.boltAnimRightSuperSpeed.play()
+#        Анімація руху в ліво     
         boltAnim = []
+        boltAnimSuperSpeed = [] 
         for anim in ANIMATION_LEFT:
             boltAnim.append((anim, ANIMATION_DELAY))
+            boltAnimSuperSpeed.append((anim, ANIMATION_SUPER_SPEED_DELAY))
         self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
         self.boltAnimLeft.play()
+        self.boltAnimLeftSuperSpeed = pyganim.PygAnimation(boltAnimSuperSpeed)
+        self.boltAnimLeftSuperSpeed.play()
         
         self.boltAnimStay = pyganim.PygAnimation(ANIMATION_STAY)
         self.boltAnimStay.play()
-        self.boltAnimStay.blit(self.image, (0, 0)) # Позамовчуванні, стоїть
+        self.boltAnimStay.blit(self.image, (0, 0)) # по замовчуванню стояти
         
         self.boltAnimJumpLeft= pyganim.PygAnimation(ANIMATION_JUMP_LEFT)
         self.boltAnimJumpLeft.play()
@@ -66,34 +79,47 @@ class Player(sprite.Sprite):
         
         self.boltAnimJump= pyganim.PygAnimation(ANIMATION_JUMP)
         self.boltAnimJump.play()
+        self.winner = False
         
 
-    def update(self, left, right, up, platforms):
+    def update(self, left, right, up, running, platforms):
         
         if up:
-            if self.onGround: # пригати, тільки коли можемо відштовнутися від землі
+            if self.onGround: # пригати якщо можемо відштовхнутися від землі
                 self.yvel = -JUMP_POWER
-            self.image.fill(Color(COLOR))
-            self.boltAnimJump.blit(self.image, (0, 0))
-               
+                if running and (left or right): # якщо є пришвидшення і ми рухаємося
+                       self.yvel -= JUMP_EXTRA_POWER # то пригаємо вище
+                self.image.fill(Color(COLOR))
+                self.boltAnimJump.blit(self.image, (0, 0))
                        
         if left:
-            self.xvel = -MOVE_SPEED # Ліво = x- n
+            self.xvel = -MOVE_SPEED # ліво = x- n
             self.image.fill(Color(COLOR))
-            if up: # для прижку в ліво є окрема анімація
-                self.boltAnimJumpLeft.blit(self.image, (0, 0))
-            else:
-                self.boltAnimLeft.blit(self.image, (0, 0))
+            if running: # якщо пришвидшення
+                self.xvel-=MOVE_EXTRA_SPEED # то передвигаемся быстрее
+                if not up: # і якщо не пригаємо
+                    self.boltAnimLeftSuperSpeed.blit(self.image, (0, 0)) #тоді відтворюємо швидку анімацію
+            else: # якщо не біжимо
+                if not up: # і не пригаємо
+                    self.boltAnimLeft.blit(self.image, (0, 0)) # тоді відображаємо анімацію руху
+            if up: # якщо пригаємо
+                    self.boltAnimJumpLeft.blit(self.image, (0, 0)) # відтворюємо анімацію прижку
  
         if right:
-            self.xvel = MOVE_SPEED # В право = x + n
+            self.xvel = MOVE_SPEED # Вправо = x + n
             self.image.fill(Color(COLOR))
-            if up:
-                self.boltAnimJumpRight.blit(self.image, (0, 0))
+            if running:
+                self.xvel+=MOVE_EXTRA_SPEED
+                if not up:
+                    self.boltAnimRightSuperSpeed.blit(self.image, (0, 0))
             else:
-                self.boltAnimRight.blit(self.image, (0, 0))
+                if not up:
+                    self.boltAnimRight.blit(self.image, (0, 0)) 
+            if up:
+                    self.boltAnimJumpRight.blit(self.image, (0, 0))
+ 
          
-        if not(left or right): # стоїмо, колиа не вказано іти
+        if not(left or right): # стоїмо коли не має вказівок іти
             self.xvel = 0
             if not up:
                 self.image.fill(Color(COLOR))
@@ -102,7 +128,7 @@ class Player(sprite.Sprite):
         if not self.onGround:
             self.yvel +=  GRAVITY
             
-        self.onGround = False; # Не знає, коли на землі(   
+        self.onGround = False;    
         self.rect.y += self.yvel
         self.collide(0, self.yvel, platforms)
 
@@ -111,20 +137,33 @@ class Player(sprite.Sprite):
    
     def collide(self, xvel, yvel, platforms):
         for p in platforms:
-            if sprite.collide_rect(self, p): # якщо пересікається платформа з гравцем
+            if sprite.collide_rect(self, p): # якщо є пересікання платформи з іграком
+                if isinstance(p, blocks.BlockDie) or isinstance(p, monsters.Monster): # якщо перескакуємо - blocks.BlockDie чи Monster
+                       self.die()# вмираємо
+                elif isinstance(p, blocks.BlockTeleport):
+                       self.teleporting(p.goX, p.goY)
+                elif isinstance(p, blocks.Princess): # якщо торнулися принцеси
+                       self.winner = True #перемогли!
+                else:
+                    if xvel > 0:                      # якщо рухається вправо
+                        self.rect.right = p.rect.left # то не рухається вліво
 
-                if xvel > 0:                      # якщо рухається в право
-                    self.rect.right = p.rect.left # то не рухається в ліво
+                    if xvel < 0:                      
+                        self.rect.left = p.rect.right 
 
-                if xvel < 0:                      # якщо рухається в ліво
-                    self.rect.left = p.rect.right 
+                    if yvel > 0:                      # якщо подає вниз
+                        self.rect.bottom = p.rect.top # то не падає в гору
+                        self.onGround = True          # якщо стає на опору
+                        self.yvel = 0                 # то енергія падіння пропадає
 
-                if yvel > 0:                      # якщо подає в низ
-                    self.rect.bottom = p.rect.top # то не подає в гору
-                    self.onGround = True          # стає на щось тверде
-                    self.yvel = 0                 # енергія подіння зникає
+                    if yvel < 0:                      # якщо рухається вверх
+                        self.rect.top = p.rect.bottom # то не рухається вниз
+                        self.yvel = 0                 # тоді енергія прижка пропадає
 
-                if yvel < 0:                      # якщо рухається верх
-                    self.rect.top = p.rect.bottom # то не рухається в низ
-                    self.yvel = 0                 #енергія прижку пропадає
-       
+    def teleporting(self, goX, goY):
+        self.rect.x = goX
+        self.rect.y = goY
+        
+    def die(self):
+        time.wait(500)
+        self.teleporting(self.startX, self.startY) # переміщення на початкові координати
